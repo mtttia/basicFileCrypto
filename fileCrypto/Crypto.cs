281289a81b91c05f4aeb84897d04012b3a94d91b
mtttia <tia369852@gmail.com>
2021-04-18 11:32:10 +0200

    finisched normal encrypt and decrypt

---
+++ fileCrypto/Crypto.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fileCrypto
+{
+    public class Crypto
+    {
+        private int _key;
+        public int Key
+        {
+            get
+            {
+                return _key;
+            }
+            set
+            {
+                if(value < 0)
+                {
+                    throw new Exception("key must be positive");
+                }
+                _key = value;
+            }
+        }
+        public string Content { get; set; }
+        public Crypto(string c, int k)
+        {
+            Content = c;
+            Key = k;
+        }
+        public string Encrypt()
+        {
+            string xorred = Xor(Content, GenerateKey(Key, Content.Length));
+            string b64 = EncodeTo64(xorred);
+            return StringToBinary(b64);
+        }
+        public string Decrypt()
+        {
+            string bin = BinaryToString(Content);
+            string b64 = DecodeFrom64(bin);
+            return Xor(b64, GenerateKey(Key, b64.Length));
+        }
+
+        private string GenerateKey(int starter, int length)
+        {
+            string s = starter.ToString();
+            Random rnd = new Random(starter);
+            int n = 9;
+            while (s.Length < length)
+            {
+                s += rnd.Next((int)Math.Pow(10, n - 1), (int)Math.Pow(10, n)); //generate number with 9 digits
+            }
+            return s;
+        }
+
+        private string Xor(string a, string b)
+        {
+            if (a.Length <= b.Length)
+            {
+                string ret = "";
+                for (int i = 0; i < a.Length; i++)
+                {
+                    char c = ToChar(ToInt(a[i]) ^ ToInt(b[i]));
+                    ret += c;
+                }
+                return ret;
+            }
+            throw new Exception("values not valid");
+        }
+        private int ToInt(char c)
+        {
+            return (int)c;
+        }
+        private char ToChar(int i)
+        {
+            return (char)i;
+        }
+        private string EncodeTo64(string toEncode)
+        {
+            byte[] toEncodeAsBytes = System.Text.ASCIIEncoding.ASCII.GetBytes(toEncode);
+            string returnValue = System.Convert.ToBase64String(toEncodeAsBytes);
+            return returnValue;
+        }
+        private string DecodeFrom64(string encodedData)
+        {
+            byte[] encodedDataAsBytes = System.Convert.FromBase64String(encodedData);
+            string returnValue = System.Text.ASCIIEncoding.ASCII.GetString(encodedDataAsBytes);
+            return returnValue;
+        }
+        private string StringToBinary(string data)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            foreach (char c in data.ToCharArray())
+            {
+                sb.Append(Convert.ToString(c, 2).PadLeft(8, '0'));
+            }
+            return sb.ToString();
+        }
+        private string BinaryToString(string data)
+        {
+            List<Byte> byteList = new List<Byte>();
+
+            for (int i = 0; i < data.Length; i += 8)
+            {
+                byteList.Add(Convert.ToByte(data.Substring(i, 8), 2));
+            }
+            return Encoding.ASCII.GetString(byteList.ToArray());
+        }
+    }
+}
--- fileCrypto/MainWindow.xaml.cs
+++ fileCrypto/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
 
 namespace fileCrypto
 {
@@ -23,6 +24,117 @@ namespace fileCrypto
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void btnEncrypt_Click(object sender, RoutedEventArgs e)
+        {
+            Encrypt(txtEncryptPath.Text, txtEncryptKey.Text);
+        }
+        private async void Encrypt(string path, string key)
+        {
+            await Task.Run(() =>
+            {
+                //create a new Crypto element
+                try
+                {
+                    //read file to get the content
+                    string content = File.ReadAllText(path);
+                    Crypto crypto = new Crypto(content, int.Parse(key));
+                    string toWrite = crypto.Encrypt();
+                    WriteFile(path + ".bin", toWrite);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            });
+        }
+
+        private async void WriteFile(string path, string content)
+        {
+            await Task.Run(() =>
+            {
+                try
+                {
+                    
+                    Byte[] write = System.Text.Encoding.ASCII.GetBytes(content);
+                    File.WriteAllBytes(path, write);
+                    
+                    MessageBox.Show("the file has been successfully encrypted, you can find it in " + path);
+                }
+                catch (Exception ex)
+                {
+                    WriteFile(path, content); //try to write file since I write it
+                }
+            });
+        }
+        private string ReadFile(string path)
+        {
+            Byte[] b = File.ReadAllBytes(path);
+            string content = System.Text.Encoding.ASCII.GetString(b);
+            return content;
+            
+        }
+
+        private void btnDecrypt_Click(object sender, RoutedEventArgs e)
+        {
+            Decrypt(txtDecryptPath.Text, txtDecryptKey.Text);
+        }
+        private async void Decrypt(string path, string key)
+        {
+            await Task.Run(() =>
+            {
+                try
+                {
+                    //read file to get the content
+
+                    string content = ReadFile(path);
+
+                    Crypto crypto = new Crypto(content, int.Parse(key));
+
+                    string toWrite = crypto.Decrypt();
+
+                    //WriteFile(DeleteBin(txtDecryptPath.Text), toWrite);
+                    path = DeleteBin(path);
+                    File.WriteAllText(path, toWrite);
+                    MessageBox.Show("the file has been successfully decrypted, you can find it in " + path);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            });
+        }
+
+        private string DeleteBin(string path)
+        {
+            if (path.Contains(".bin"))
+            {
+                string[] p = path.Split('.');
+                string ret = "";
+                for(int i = 0; i < p.Length; i++)
+                {
+                    if(i != p.Length-1)
+                    {
+                        if(i == p.Length-2)
+                        {
+                            //there is no '.' at the end
+                            ret += p[i];
+                        }
+                        else
+                        {
+                            ret += p[i] + ".";
+                        }
+                        
+                    }
+                }
+                return ret;
+            }
+            else
+            {
+                throw new Exception("this is not a file encrypte with this app");
+            }
+            
         }
     }
 }
